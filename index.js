a940fb8b5acebaa9577bfa615202603c2024e272
Nick Lee <leexnick@gmail.com>
2019-06-12 11:29:15 -0700

    finish Array class

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import Array from './Array'
+const Array = require('./Array')
 
 function main() {
+  Array.SIZE_RATIO = 3;
   
+  let arr = new Array();
+
+  arr.push(3);
+
+  console.log(arr);
 }
+
+main();
